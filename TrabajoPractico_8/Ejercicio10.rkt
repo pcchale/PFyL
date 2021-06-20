5cc06ccd1a96b04c396172f6d19a14cdd9b032ef
Agustin Normand <NormandAgustin@gmail.com>
2021-06-19 22:28:06 -0300

    Subo el ejercicio 10 del tp8

--- TrabajoPractico_8/Ejercicio10.rkt
+++ TrabajoPractico_8/Ejercicio10.rkt
@@ -1,13 +1,20 @@
 #lang racket
 
-(define (nPrimeros cantidad lista)
-    (nPrimerosAux cantidad lista 0 '())
-)
+(define (nPrimerosRecursividadCola cantidad lista)
+        (nPrimerosAux cantidad lista 0 '()))
 
 (define (nPrimerosAux cantidad lista cantidadActual listaAcumuladora)
-    (cond
-        ((= cantidad cantidadActual) listaAcumuladora)
-        ((null? lista) listaAcumuladora)
-        (else (nPrimerosAux cantidad (cdr lista) (+ cantidadActual 1) (list listaAcumuladora (car lista))))
-    )
-)
+        (cond ((= cantidad cantidadActual) listaAcumuladora)
+              ((null? lista) listaAcumuladora)
+              (else
+               (nPrimerosAux cantidad
+                             (cdr lista)
+                             (+ cantidadActual 1)
+                             (append listaAcumuladora (list (car lista)))))))
+                            #|Esto está medio confuso, lo intuitivo era (list listaAcumuladora (car lista))
+                              pero eso devolvía parentesis de más y quedaba resprolijo |#
+
+(define (nPrimerosRecursividadPila cantidadElementos lista)
+        (cond ((= cantidadElementos 0) '())
+              ((null? lista) '())
+              (else (cons (car lista) (nPrimerosP (- cantidadElementos 1) (cdr lista))))))
